ce137f68845930398fc98fa41cb0f516d5d61619
Arpit <arpit@racloop.com>
2022-07-05 10:57:12 +0530

    New Files

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String args[])
+    {
+        Car porche = new Car();
+        Car holden = new Car();
+        //porche.model="Carrera";
+        porche.setModels("Carrera");
+        System.out.println(porche.getModels());
+    }
+//        int newScore = Calc("Appy",100);
+//        Calc("Arpit",10);
+//        Calc(100);
+//
+//    }
+//    public static int Calc(String Name, int score)
+//    {
+//        System.out.println("Name: "+ Name + "Score: " + score);
+//        return score*1000;
+//
+//    }
+//    public static int Calc(int score)
+//    {
+//        System.out.println("Unnamed Name Score: " + score);
+//        return score*1000;
+//
+//    }
+
+}
