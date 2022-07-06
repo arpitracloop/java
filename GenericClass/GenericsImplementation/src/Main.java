7215748c0156b4386f34a6636812c4d80468e8be
Arpit <arpit@racloop.com>
2022-07-06 18:40:06 +0530

    Generics Final

--- GenericClass/GenericsImplementation/src/Main.java
+++ GenericClass/GenericsImplementation/src/Main.java
@@ -4,12 +4,41 @@ public class Main {
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-        Team mantud = new Team(("Mantud"));
+        Team<FootballPlayer> mantud = new Team<>(("Mantud"));
         mantud.addPlayer(joe);
-        mantud.addPlayer(pat);
-        mantud.addPlayer(beckham);
+        //mantud.addPlayer(pat);
+        //mantud.addPlayer(beckham);
 
         System.out.println(mantud.numPlayers());
+
+        Team<BaseballPlayer> coerbasketballteam = new Team<>("CoerBaskballTeam");
+        coerbasketballteam.addPlayer(pat);
+
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("error");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> Chealsea = new Team<>("Chealsea");
+        FootballPlayer banks = new FootballPlayer("Messy");
+        Chealsea.addPlayer(banks);
+
+        Team<FootballPlayer> RealMadrid = new Team<>("RealMadrid");
+        Team<FootballPlayer> Finland = new Team<>("Finland");
+        Chealsea.matchResult(Finland, 1,0);
+        Chealsea.matchResult(mantud,3,8);
+
+        mantud.matchResult(Chealsea,2,1);
+        mantud.matchResult(Finland,2,2);
+
+        System.out.println("Rankings...");
+        System.out.println(mantud.getName() + " : " + mantud.ranking()  );
+        System.out.println(Chealsea.getName() + " : " + Chealsea.ranking()  );
+        System.out.println(Finland.getName() + " : " + Finland.ranking()  );
+
+
+        System.out.println(mantud.compareTo(Finland));
+        System.out.println(mantud.compareTo(Chealsea));
+    }
     }
 //    public static void main(String[] args) {
 //        ArrayList<Integer> items = new ArrayList();
@@ -29,6 +58,6 @@ public class Main {
 //            System.out.println((Integer)i*2);
 //        }
 //    }
-}
+
 
 
