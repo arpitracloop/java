ac8143b98a68515ce648de2fdde447fa81bd2b1b
Arpit <arpit@racloop.com>
2022-07-12 12:15:54 +0530

    Lambda Expression 4

--- Lambdas/Lambdas/src/Main.java
+++ Lambdas/Lambdas/src/Main.java
@@ -2,22 +2,48 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+class Employee
+{
+    private String name;
+    private int age;
+    //Constructor
+    public Employee(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    //Getters and Setters
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
 //        new Thread(()->{
 //            System.out.println("Printing from the Runnable");
 //            //System.out.format("Printing line %d from Runnable",2);
-//
-//
 //        }).start();
+        Employee john   = new Employee("John",22);
+        Employee harry  = new Employee("Harry",21);
+        Employee jack   = new Employee("Jack",23);
+        Employee don    = new Employee("Don",25);
 
-        Employee john = new Employee("John",22);
-        Employee harry = new Employee("Harry",21);
-        Employee jack = new Employee("Jack",23);
-        Employee don = new Employee("Don",25);
+        List <Employee> employees = new ArrayList<>();
 
-        List<Employee> employees = new ArrayList<>();
         employees.add(john);
         employees.add(harry);
         employees.add(jack);
@@ -37,7 +63,7 @@ public class Main {
         {
             System.out.println(employee.getName());
         }
-//
+
 //        String sillyString = doStringStuff(new UpperConcat() {
 //            @Override
 //            public String upperAndConcat(String s1, String s2) {
@@ -47,9 +73,15 @@ public class Main {
 //        employees.get(0).getName(),employees.get(1).getName());
 //        System.out.println(sillyString);
 
-        UpperConcat uc = ( s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
-        uc = ( s3, s4) -> s3.toUpperCase() + s4.toUpperCase();
 
+        //UpperConcat uc =(s1,s2)-> s1.toUpperCase()+s2.toUpperCase();  to use one expression in lambda expression
+
+
+        UpperConcat uc = ( s1, s2) -> {                               // to use multiple statement in lambda expression
+            String result = s1.toUpperCase() + " " + s2.toUpperCase();
+            return result;
+            //uc = ( s3, s4) -> s3.toUpperCase() +" "+ s4.toUpperCase();
+        };
         String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
         System.out.println(sillyString);
 
@@ -59,40 +91,6 @@ public class Main {
     public final static  String doStringStuff(UpperConcat uc,String s1, String s2)
     {
         return uc.upperAndConcat(s1,s2);
-    }
-}
-
-class Employee
-{
-    private String name;
-    private int age;
-
-    //Constructor
-    public Employee(String name, int age)
-    {
-        this.name = name;
-        this.age = age;
-    }
-
-    //Getters and Setters
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public int getAge()
-    {
-        return age;
-    }
-
-    public void setAge(int age)
-    {
-        this.age = age;
     }
 }
 
