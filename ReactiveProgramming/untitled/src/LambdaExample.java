7b861098e62cb0124c1b199d06b83a6d2c818738
Arpit <arpit@racloop.com>
2022-09-12 12:36:03 +0530

    Functional Interface

---
+++ ReactiveProgramming/untitled/src/LambdaExample.java
@@ -0,0 +1,21 @@
+public class LambdaExample {
+	public static void main(String[] args) {
+
+//		MyRunnable runnable = new MyRunnable();
+//		Thread t = new Thread(new Runnable() {
+//			@Override
+//			public void run() {
+//				System.out.println("In anonymous class");
+//			}
+//		});
+//		t.start();
+
+      // Lambda method
+//		Thread t = new Thread(()-> System.out.println("In lambda thread method"));
+//		t.start();
+
+		FunctionalInterface functionalInterface = ()-> System.out.println("Hello from functional interface");
+		functionalInterface.myMethod();
+	}
+
+}
